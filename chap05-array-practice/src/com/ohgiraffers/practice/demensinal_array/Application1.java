45a767aa50c4fab4b28b2c47b526dd5a7f91679b
yunjiniius <yunjiniius@gmail.com>
2023-12-20 17:37:21 +0900

    2023-12-20 practice for chap5 (array)

---
+++ chap05-array-practice/src/com/ohgiraffers/practice/demensinal_array/Application1.java
@@ -0,0 +1,21 @@
+package com.ohgiraffers.practice.demensinal_array;
+
+public class Application1 {
+
+    public static void main(String[] args) {
+
+        /* 1. 행의 길이가 3, 열의 길이가 4인 정수형 2차원 배열을 선언 및 할당
+         * 2. 각 인덱스에 차례대로 1부터 12까지의 정수 대입
+         * 3. 그 값을 출력하는 코드를 작성하세요.
+         *
+         * -- 출력 예시 --
+         * 1  2  3  4
+         * 5  6  7  8
+         * 9 10 11 12
+         *
+         * 단, 출력물의 줄맞춤은 신경쓰지 말고 풀어보세요.
+         * */
+
+    }
+
+}
